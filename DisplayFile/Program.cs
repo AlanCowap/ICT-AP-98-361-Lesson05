c6eddc96f1e5d20add06223d705b23380737cf25
Alan Cowap <spam@alancowap.com>
2016-10-19 13:52:28 +0100

    Add Console Application, cmd line args

---
+++ DisplayFile/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace DisplayFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Please enter a valid filename as argument e.g. >DisplayFile sample.txt");
+                return;
+            }
+            string[] lines = File.ReadAllLines(args[0]);
+            foreach(String item in lines)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
